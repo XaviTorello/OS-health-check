40559569e34407ecd7f71b9edb0c1a681d14fa69
Xavi Torelló <info@xaviertorello.cat>
2016-02-28 17:59:48 +0100

    Class Connection created. Pending to catch Connection exceptions

--- os-health-check.py
+++ os-health-check.py
@@ -1,1 +1,66 @@
+#/usr/bin/python
+
+import paramiko
+
+
+
+class Connection ():
+    """ Connection object
+    """
+    client = paramiko.SSHClient()
+    last_command = []
+
+
+    def __init__(self, host, user, passwd, port=22):
+        self.set_new_connection(host,user,passwd, port)
+
+    def set_new_connection(self, host, user, passwd, port):
+        try:
+            self.close_connection()
+        except:
+            print "Error tancant"
+
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.set_missing_host_key_policy(paramiko.WarningPolicy())
+
+
+        self.client.connect(host, port=port, username=user, password=passwd)
+
+        ## Pending try connection except host, usr/pswd, ...
+        #try:
+        #except "AuthenticationException":
+        #    print "Authentication failed"
+        #    raise "AuthenticationFailed"
+        #except:
+        #    print "error"
+        #    self.client.close()
+
+
+    def launch_command(self,command):
+        #last_command = command
+        stdin, stdout, stderr = self.client.exec_command(command)
+        self.last_command =  [command, stdin, stdout, stderr]
+        return self.last_command
+
+    def print_last_command (self):
+        for line in self.last_command[2]:
+            print line.strip('\n')
+
+    def close_connection(self):
+        self.client.close()
+
+
+
+user="user"
+password="password"
+host="localhost"
+
+local = Connection(host, user, password)
+
+command = local.launch_command("cat /proc/cpuinfo")
+
+local.print_last_command()
+
+local.close_connection()
 
